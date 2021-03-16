f8f36d5834c1f119a002f9a8bf558ba7c055ce16
vvkarpov <chuzhoi36@gmail.com>
2021-03-16 15:37:48 +0300

    initial commit

---
+++ src/main/java/ru/vkarpov/dev/fbolts/FboltsApplication.java
@@ -0,0 +1,13 @@
+package ru.vkarpov.dev.fbolts;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class FboltsApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(FboltsApplication.class, args);
+    }
+
+}
