49e8732de7efecbd370e84d1038a8a6efa0c8e57
vvkarpov <chuzhoi36@gmail.com>
2021-04-01 15:14:45 +0300

    add enum diameters for foundation bolts type 1.1

---
+++ src/main/java/ru/vkarpov/dev/fbolts/service/type1d1/Diameter_1d1.java
@@ -0,0 +1,18 @@
+package ru.vkarpov.dev.fbolts.service.type1d1;
+
+public enum Diameter_1d1 {
+    D12(12),
+    D16(16),
+    D20(20),
+    D24(24),
+    D30(30),
+    D36(36),
+    D42(42),
+    D48(48);
+
+    public final int count;
+
+    Diameter_1d1(int count) {
+        this.count = count;
+    }
+}
