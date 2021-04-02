1ec60bcc1d95fa924934a55717f88052b29c9ebb
vvkarpov <chuzhoi36@gmail.com>
2021-04-02 10:11:17 +0300

    add enum CurvedLength for foundation bolts type 1.1

---
+++ src/main/java/ru/vkarpov/dev/fbolts/service/type1d1/CurvedLength_1d1.java
@@ -0,0 +1,18 @@
+package ru.vkarpov.dev.fbolts.service.type1d1;
+
+public enum CurvedLength_1d1 {
+    D12(40),
+    D16(50),
+    D20(60),
+    D24(75),
+    D30(90),
+    D36(110),
+    D42(125),
+    D48(150);
+
+    public final int count;
+
+    CurvedLength_1d1(int count) {
+        this.count = count;
+    }
+}
